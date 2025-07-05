fe9f5874b778725152153e850ebf9dffedd0f666
GamingLuke1337 <64887613+GamingLuke1337@users.noreply.github.com>
2025-07-05 12:24:30 +0200

    Update and rename next.config.js to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,15 @@
-const withNextra = require('nextra')({
+import nextra from 'nextra'
+
+const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
   staticImage: true,
-});
+})
 
-module.exports = {
+export default {
   ...withNextra(),
   images: {
     unoptimized: true,
   },
   // basePath: '/tokovoip_v2-docs',
-};
+}
